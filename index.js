fac75c178dbf1e7e5226ca316ea1403cc03b051b
xaviguasch <xaviguasch@gmail.com>
2020-08-31 11:51:26 +0200

    set the game state

---
+++ index.js
@@ -0,0 +1,9 @@
+const p1ScoreBoard = document.getElementById('player1Scoreboard')
+const p2ScoreBoard = document.getElementById('player2Scoreboard')
+const p1Dice = document.getElementById('player1Dice')
+const p2Dice = document.getElementById('player2Dice')
+const rollBtn = document.getElementById('rollBtn')
+
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
